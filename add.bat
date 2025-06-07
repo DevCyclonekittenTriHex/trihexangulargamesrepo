d941d689492894a161525c55ed60f1b1ee5cf8c6
Cyconekitten <157323498+DevCyclonekittenTriHex@users.noreply.github.com>
2025-06-07 10:44:50 +1000

    lfs

---
+++ add.bat
@@ -0,0 +1,5 @@
+git lfs track "Square Adventure.zip"
+git lfs push --all origin main
+git add .
+git commit -m "lfs"
+git push -u origin main
---
+++ setup.bat
@@ -0,0 +1,4 @@
+git checkout -b main
+touch README.md  # Create a new file
+git add README.md
+git commit -m "Initial commit"
